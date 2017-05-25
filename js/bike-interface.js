0da868b015265cd9b38012098e066d9c589896cd
jin camou and sara schultz <sara@>
2017-05-25 14:42:31 -0700

    refactor to include callback function

--- js/bike-interface.js
+++ js/bike-interface.js
@@ -1,14 +1,15 @@
 var Bike = require('./../js/bike.js').bikeModule;
 
-// var displayLocations = function(locationData) {
-//   console.log(locationData)
-//   // $('.showLocation').text('Stolen bikes: ' + locationData);
-// }
+var displayLocations = function(response) {
+  response.bikes.forEach(function(bike) {
+    $('.showLocation').append('<li>' + 'Location: ' + bike.stolen_location + '</li>');
+  });
+}
 
 $(document).ready(function() {
   $('#location').click(function() {
     var currentBikeObject = new Bike();
     var city = $('#city').val();
-    currentBikeObject.getLocation(city);
+    currentBikeObject.getLocation(city, displayLocations);
   });
 });
--- js/bike.js
+++ js/bike.js
@@ -1,14 +1,10 @@
 Bike = function() {
 }
 
-Bike.prototype.getLocation = function(city) {
+Bike.prototype.getLocation = function(city, displayLocations) {
   $.get('https://bikeindex.org:443/api/v3/search?page=1&per_page=25&location=' + city + '&distance=10&stolenness=proximity')
     .then(function(response) {
-      response.bikes.forEach(function(bike) {
-        // console.log(response.bikes[0].stolen_location);
-        $('.showLocation').append('<li>' + 'Location: ' + bike.stolen_location + '</li>');
-      // displayLocations(response.bikes.stolen_location);
-      })
+      displayLocations(response)
     }).fail(function(error) {
     $('.showLocation').text(error.responseJSON.message);
   });
