be25aac6c7a491d320d1bc276e2fb78083e30cfe
jin camou and sara schultz <sara@>
2017-05-25 14:52:57 -0700

    add manufacturer info to list

--- js/bike.js
+++ js/bike.js
@@ -2,7 +2,7 @@ Bike = function() {
 }
 
 Bike.prototype.getLocation = function(city, displayLocations) {
-  $.get('https://bikeindex.org:443/api/v3/search?page=1&per_page=25&location=' + city + '&distance=10&stolenness=proximity')
+  $.get('https://bikeindex.org:443/api/v3/search?page=1&per_page=25&location=' + city +  '&distance=10&stolenness=proximity')
     .then(function(response) {
       displayLocations(response)
     }).fail(function(error) {
