4e6c8985f8d4a35de31fe6a36cb05a37030c219e
jin camou and sara schultz <jin@>
2017-05-25 17:00:39 -0700

    display images

--- js/bike-interface.js
+++ js/bike-interface.js
@@ -2,9 +2,9 @@ var Bike = require('./../js/bike.js').bikeModule;
 
 var displayLocations = function(response) {
   response.bikes.forEach(function(bike) {
-    $('.showLocation').append(`<li>Location:${bike.stolen_location}Manufacturer: ${bike.manufacturer_name}<img src="${bike.thumb}"/></li>`);
+    $('.showLocation').append(`<li>Location: ${bike.stolen_location}</li><li>Manufacturer:  ${bike.manufacturer_name}</li><li>Model: ${bike.frame_model}</li><li><img src="${bike.thumb}"/></li><br>`);
   });
-}
+};
 
 $(document).ready(function() {
   $('#location').click(function() {
--- js/map-interface.js
+++ js/map-interface.js
@@ -1,5 +1,5 @@
 // var Map = require('./../js/map.js').mapModule;
-//
+
 // $(document).ready(function() {
 //   $('#location').click(function() {
 //     var city = $('#city').val();
